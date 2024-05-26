4c21d94e685810765dbc7cb757d20c12d74c9a16
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-26 18:53:56 +0530

    CreateBrowserRouter created so that we can use powerful apis like data fetcher and datad loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import Home from "./ui/Home";
+import CreateOrder from "./features/order/CreateOrder";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+
+import { createBrowserRouter ,RouterProvider} from "react-router-dom";
+import Order from "./features/order/Order";
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  { path: "/cart", element: <Cart /> },
+  { path: "/order/new", element: <CreateOrder /> },
+  { path: "/order/:orderId", element: <Order /> },
+]);
 
 const App = () => {
-  return <div>App</div>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
