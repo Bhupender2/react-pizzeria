73515a49b0ef71cf4debeee4bfd6f9d71e191c0d
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-31 13:03:36 +0530

    data is fetched using react router loader function and connect the function in the route using loader property and then get te data using useLoaderDatat

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,13 @@ import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Order from "./features/order/Order";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
     errorElement: <Error />,
-
     children: [
       { path: "/", element: <Home /> },
       {
@@ -22,11 +21,20 @@ const router = createBrowserRouter([
         errorElement: <Error />,
       },
       { path: "/cart", element: <Cart /> },
-      { path: "/order/new", element: <CreateOrder /> },
-      { path: "/order/:orderId", element: <Order /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+      },
+      {
+        path: "/order/:orderId",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+      },
     ],
   },
 ]);
+// we put the error element on the parent route coz all the error will get bubbles up unless it was handled individually inside the nested route
 
 //error in the nested routes will actually bubbles up to the parent route unless they are handled individually in the nested route using errorElement property.
 
--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -7,7 +7,7 @@ export default function AppLayout() {
   const navigation = useNavigation(); // making an instance so that we can use loading state provided by useNavigation hook
 
   console.log(navigation);
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === "loading";  // this laoding indicator is universal to whole app so it any page that is still loading it will be loading unless all the pages are render
   return (
     <div className="layout">
       {isLoading && <Loader />}
