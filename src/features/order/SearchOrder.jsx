4aae6e56f0ce04781f7101459bd128f447ce7829
Bhupender2 <b.mehtaa2021@gmail.com>
2024-05-31 12:30:53 +0530

    here i created a  form and submitted the result on clicking and any other functionality

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const SearchOrder = () => {
+  const [query, setQuery] = useState("");
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!query) return;
+    navigate(`/order/${query}`);
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      {/*on submitting the data on enter we can use form so that on pressing enter(return) we can submit the data and do whatever functionality that we want to use*/}
+      <input
+        type="text"
+        placeholder="search order #"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+    </form>
+  );
+};
+
+export default SearchOrder;
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 export default function Header() {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
       <p>Bhupender</p>
+      <SearchOrder/>
     </header>
   );
 }
