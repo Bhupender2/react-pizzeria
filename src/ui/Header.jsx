a654d76ea410d14473731ff6bc20cea467e8f310
Bhupender2 <b.mehtaa2021@gmail.com>
2024-06-03 01:40:26 +0530

    how to give space betwwen twro obkects

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,10 +4,12 @@ import Username from "../features/user/Username";
 
 export default function Header() {
   return (
-    <header className="bg-yellow-500 uppercase">
-      <Link to="/" className="tracking-widest">Fast React Pizza Co.</Link>
-      <SearchOrder/>
-      <Username/>
+    <header className="border-b border-stone-200 bg-yellow-500 px-4 py-3 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React Pizza Co.
+      </Link>
+      <SearchOrder />
+      <Username />
     </header>
   );
 }
