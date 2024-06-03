9d4e9523bb65887fd47b3a737d11e8c64335fd87
Bhupender2 <b.mehtaa2021@gmail.com>
2024-06-04 02:53:07 +0530

    theme(colors.stone.800)

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,10 +10,10 @@ export default function AppLayout() {
   const isLoading = navigation.state === "loading"; // this laoding indicator is universal to whole app so it any page that is still loading it will be loading unless all the pages are render
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      {/* {isLoading && <Loader />} */}
-      {true &&<Loader/>}
+      {isLoading && <Loader />}
+
       <Header />
-      <div className="overflow-scroll ">
+      <div className="overflow-scroll">
         <main className="mx-auto max-w-3xl">
           <Outlet />
         </main>
