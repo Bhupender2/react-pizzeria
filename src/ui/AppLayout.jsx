3503407c59112e37a00c37db03dc405235cf7f6f
Bhupender2 <b.mehtaa2021@gmail.com>
2024-06-04 02:50:23 +0530

    loader is made using backdrop-blur-sm inset-0 absolute bg-slate-200/20

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -10,7 +10,8 @@ export default function AppLayout() {
   const isLoading = navigation.state === "loading"; // this laoding indicator is universal to whole app so it any page that is still loading it will be loading unless all the pages are render
   return (
     <div className="grid h-screen grid-rows-[auto_1fr_auto]">
-      {isLoading && <Loader />}
+      {/* {isLoading && <Loader />} */}
+      {true &&<Loader/>}
       <Header />
       <div className="overflow-scroll ">
         <main className="mx-auto max-w-3xl">
