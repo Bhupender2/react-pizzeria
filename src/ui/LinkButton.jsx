c8c3804a279e349d0ae5e3c94d7e83279c46d88c
Bhupender2 <b.mehtaa2021@gmail.com>
2024-06-04 02:03:08 +0530

    formatting done

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -5,9 +5,11 @@ function LinkButton({ to, children }) {
   const navigate = useNavigate();
   const className = "text-sm text-blue-500 hover:text-blue-600 hover:underline";
   if (to === "-1")
-   return <button className={className} onClick={() => navigate(-1)}>
-      {children}
-    </button>;
+    return (
+      <button className={className} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
   return (
     <Link to={to} className={className}>
       {children}
