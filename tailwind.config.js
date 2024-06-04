10fe222db494bda4c08927b8936e99629ccdaa81
Bhupender2 <b.mehtaa2021@gmail.com>
2024-06-04 15:58:52 +0530

    font family added and overide the tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [ "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: "roboto monospace",
+    }, // we override the sans default config given by tailwindcss configuration by giving our font family (given the link in index.html)
     extend: {},
   },
   plugins: [],
-}
-
+};
