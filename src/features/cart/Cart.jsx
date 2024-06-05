8db40e66d2474f12d0b80828fdd7548c3142b091
Bhupender2 <b.mehtaa2021@gmail.com>
2024-06-06 02:36:07 +0530

    cart is completed

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -44,7 +44,7 @@ function Cart() {
         <Button to="/order/new" type="primary">
           Order Pizza
         </Button>
-        <button>Clear cart</button>
+        <Button type="secondary">Clear cart</Button>
       </div>
     </div>
   );
