446f84f7ac34ef049968cd6867a1abb84fcb1bc1
Bhupender2 <b.mehtaa2021@gmail.com>
2024-06-06 02:28:28 +0530

    cart layout change

--- src/features/cart/Cart.jsx
+++ src/features/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import Button from "../../ui/Button";
 import LinkButton from "../../ui/LinkButton";
+import CartItem from "./CartItem";
 
 const fakeCart = [
   {
@@ -30,16 +31,19 @@ function Cart() {
 
   return (
     <div>
-      <LinkButton
-        to="/menu"
-      >
-        &larr; Back to menu
-      </LinkButton>
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <h2>Your cart, %NAME%</h2>
+      <h2 className="mt-7 text-xl font-semibold">Your cart, %NAME%</h2>
+      <ul className="mt-3 divide-y divide-stone-200 border-b">
+        {cart.map((item) => (
+          <CartItem item={item} key={item.key} />
+        ))}
+      </ul>
 
-      <div>
-        <Button to="/order/new" type="primary">Order Pizza</Button>
+      <div className="mt-6 space-x-2">
+        <Button to="/order/new" type="primary">
+          Order Pizza
+        </Button>
         <button>Clear cart</button>
       </div>
     </div>
