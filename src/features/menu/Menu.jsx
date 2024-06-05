6b082b1d662f9bc5ec92337323ca66ee9d69e39c
Bhupender2 <b.mehtaa2021@gmail.com>
2024-06-05 23:11:19 +0530

    some chnages done

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -5,7 +5,8 @@ import MenuItem from "./MenuItem";
 function Menu() {
   const menu = useLoaderData(); // to load the data coming from menu route
   return (
-    <ul className="divide-y divide-stone-200 px-2"> {/*we can create line between child element*/}
+    <ul className="divide-y divide-stone-200 px-2">
+      {/*we can create line between child element*/}
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
