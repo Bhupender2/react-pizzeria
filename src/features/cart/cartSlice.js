cf9819b5349f8ae786ff89ba67a70f4ab75f4b38
Bhupender2 <b.mehtaa2021@gmail.com>
2024-07-12 00:50:17 +0530

    reducers are made

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -17,10 +17,35 @@ const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    addItem(state, action) {},  //these are the five reducers
-    deleteItem(state, action) {},
-    increaseItemQuantity(state, action) {},
-    decreaseItemQuantity(state, action) {},
-    clearItem(state, action) {},
+    addItem(state, action) {
+      state.push(action.payload); //pushing the item straight away(newItem===payload)
+    }, //these are the five reducers
+    deleteItem(state, action) {
+      //pizzaId===payload
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload); //filter the item whose id is matched to the payload
+    },
+    increaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity++; // here on the this value has already been updated
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
+    decreaseItemQuantity(state, action) {
+      const item = state.cart.find((item) => item.pizzaId === action.payload);
+      item.quantity--; // here on the this value has already been updated
+      item.totalPrice = item.unitPrice * item.quantity;
+    },
+    clearItem(state) {
+      state.cart = [];
+    },
   },
 });
+
+export const {
+  addItem,
+  deleteItem,
+  increaseItemQuantity,
+  decreaseItemQuantity,
+  clearItem,
+} = cartSlice.actions; //actions creator are created automatically
+
+export default cartSlice.reducer; // exporting the combine reducer done by createSlice
