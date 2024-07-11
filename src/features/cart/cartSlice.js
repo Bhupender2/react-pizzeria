70497a8741ba5f94cbbeddb0b20ecb832aeb9524
Bhupender2 <b.mehtaa2021@gmail.com>
2024-07-12 00:27:27 +0530

    slice is created and initialState is defined

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,9 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
-const initialState={
-  cart:[]
-}   // deriving the initial state 
+const initialState = {
+  // cart:[]
+  cart: [
+    {
+      pizzaId: 12,
+      name: "Mediterranean",
+      quantity: 2,
+      unitPrice: 16,
+      totalPrice: 32,
+    },
+  ],
+}; // deriving the initial state
 
-const cartSlice= createSlice({
-  
-})
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem(state, action) {},  //these are the five reducers
+    deleteItem(state, action) {},
+    increaseItemQuantity(state, action) {},
+    decreaseItemQuantity(state, action) {},
+    clearItem(state, action) {},
+  },
+});
