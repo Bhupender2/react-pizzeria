8108de4bad71859a482b2f76452cedade209e266
Bhupender2 <b.mehtaa2021@gmail.com>
2024-07-16 01:21:32 +0530

    format the code here only

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -55,7 +55,7 @@ export const getTotalCartQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
-
+   
 export const getCart = (state) => state.cart.cart;
 
 export const getCurrentQuantityById = (id) => (state) =>
