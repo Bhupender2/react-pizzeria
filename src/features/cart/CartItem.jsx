802fcdc6fb4256ac84f6b2bcccb87fe0d49597b5
Bhupender2 <b.mehtaa2021@gmail.com>
2024-07-16 01:34:24 +0530

    dispatch action on increasing and decreasing on + -

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -3,19 +3,22 @@ import Button from "../../ui/Button";
 import { formatCurrency } from "../../utils/helpers";
 import { deleteItem } from "./cartSlice";
 import DeleteItem from "./DeleteItem";
+import UpdateItemByQuantity from "./UpdateItemByQuantity";
 
 function CartItem({ item }) {
   const { pizzaId, name, quantity, totalPrice } = item;
-  const dispatch= useDispatch()
+  const dispatch = useDispatch();
 
   return (
     <li className="py-3 sm:flex sm:items-center sm:justify-between">
       <p className="mb-1 sm:mb-0">
         {quantity}&times; {name}
       </p>
-      <div className="flex items-center justify-between sm:gap-6"> 
+      <div className="flex items-center justify-between sm:gap-6">
         <p className="text-sm font-bold">{formatCurrency(totalPrice)}</p>
-        <DeleteItem pizzaId={pizzaId}/>
+
+        <UpdateItemByQuantity pizzaId={pizzaId} />
+        <DeleteItem pizzaId={pizzaId} />
       </div>
     </li>
   );
---
+++ src/features/cart/UpdateItemByQuantity.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import Button from "../../ui/Button";
+import { decreaseItemQuantity, increaseItemQuantity } from "./cartSlice";
+
+function UpdateItemByQuantity({pizzaId}) {
+  const dispatch=useDispatch()
+
+  return (
+    <div className="flex gap-1 item-center ">
+      <Button type="round" onClick={()=>dispatch(increaseItemQuantity(pizzaId))}>+</Button>
+      <Button type="round" onClick={()=>dispatch(decreaseItemQuantity(pizzaId))}>-</Button>
+    </div>
+  );
+}
+
+export default UpdateItemByQuantity;
