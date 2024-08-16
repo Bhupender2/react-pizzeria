920b7e98eb8b3835537915588923bd9799f06116
bhupender <b.mehtaa2021@gmail.com>
2024-08-16 17:50:40 +0530

    createAsyncThunk is created

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -1,51 +1,55 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
+import { getAddress } from "../../services/apiGeocoding";
 
-const initialState={
-  username:""
-}
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
-const userSlice= createSlice({
-  name:"user",
+const initialState = {
+  username: "",
+};
+
+const userSlice = createSlice({
+  name: "user",
   initialState,
-  reducers:{
-    updateName(state, action){
-      state.username=action.payload  // we can directly mutate the state in redux toolkit.
-    }
-  }
-})
+  reducers: {
+    updateName(state, action) {
+      state.username = action.payload; // we can directly mutate the state in redux toolkit.
+    },
+  },
+});
 
-export const {updateName} = userSlice.actions
+export const { updateName } = userSlice.actions;
 
 export default userSlice.reducer;
 
-// function getPosition() {
-//   return new Promise(function (resolve, reject) {
-//     navigator.geolocation.getCurrentPosition(resolve, reject);
-//   });
-// }
+function getPosition() {
+  // wrapping a promise in a function so that we can use async await coz it returns a promise when we use async await
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+} // here we get the information of the current position and then based on the current position we find the address and everything
 
-// async function fetchAddress() {
-//   // 1) We get the user's geolocation position
-//   const positionObj = await getPosition();
-//   const position = {
-//     latitude: positionObj.coords.latitude,
-//     longitude: positionObj.coords.longitude,
-//   };
+//fetchAddress has become the action creator function which we later call on
 
-//   // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
-//   const addressObj = await getAddress(position);
-//   const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+export const fetchAddress = createAsyncThunk("user/fetchAddress", async function () {
+  // 1) We get the user's geolocation position
+  const positionObj = await getPosition();
+  const position = {
+    latitude: positionObj.coords.latitude,
+    longitude: positionObj.coords.longitude,
+  };
 
-//   // 3) Then we return an object with the data that we are interested in
-//   return { position, address };
-// }
+  // 2) Then we use a reverse geocoding API to get a description of the user's address, so we can display it the order form, so that the user can correct it if wrong
+  const addressObj = await getAddress(position);
+  const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
 
+  // 3) Then we return an object with the data that we are interested in
+  return { position, address };
+}); // passing an async function which will return the payload for the reducer later  ( this function will return a promise so an async function is perfect here)
 
+// MIDDLEWARE
+// it sits between dispatching and reducer itself it will do something to the dispactched action before actually updating the store
 
+// there are two ways of
+//1. manually creating our own action creator and placed the thunk in there
 
-
-
-
-
-
-
+// 2 .now in order to create a thunk we will use createAsyncThunk
--- src/services/apiGeocoding.js
+++ src/services/apiGeocoding.js
@@ -1,7 +1,7 @@
 export async function getAddress({ latitude, longitude }) {
   const res = await fetch(
-    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`
-  );
+    `https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`  // reverse geocoding ---is like getting some information based on the position of the location (latitude and longitude)
+  ); 
   if (!res.ok) throw Error("Failed getting address");
 
   const data = await res.json();
