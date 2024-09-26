0f29761dcd1f70dbb97551f3a741f81c479f96c1
bhupender <b.mehtaa2021@gmail.com>
2024-09-26 18:29:24 +0530

    get position button is added on the input adress

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -39,7 +39,6 @@ function CreateOrder() {
   return (
     <div className="px-4 py-6">
       <h2 className="mb-8 text-xl font-semibold">Ready to order? Let's go!</h2>
-      <button onClick={() => dispatch(fetchAddress())}>get postion</button> 
 
       {/* <Form method="POST" action="order/new"> dont need to specify the action where the submission should be submitted the react-router is smart enough  */}
       <Form method="POST">
@@ -66,7 +65,7 @@ function CreateOrder() {
             )}
           </div>
         </div>
-        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+        <div className="relative mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">Address</label>
           <div className="grow">
             <input
@@ -76,6 +75,11 @@ function CreateOrder() {
               required
             />
           </div>
+          <span className="absolute right-[3px] z-50">
+            <Button type="small" onClick={() => dispatch(fetchAddress())}>
+              get postion
+            </Button>
+          </span>
         </div>
         <div className="mb-12 flex items-center gap-5">
           <input
