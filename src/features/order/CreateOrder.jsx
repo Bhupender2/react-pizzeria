d104a081253918760023196e665e1a8f2437d128
Bhupender2 <b.mehtaa2021@gmail.com>
2024-09-30 20:23:52 +0530

    error msg is added

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -18,7 +18,14 @@ const isValidPhone = (str) =>
 
 function CreateOrder() {
   const [withPriority, setWithPriority] = useState(false);
-  const username = useSelector((state) => state.user.username);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address, // using this address fetched address as a defualt value
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === "loading";
 
   const navigation = useNavigation();
 
@@ -73,16 +80,29 @@ function CreateOrder() {
               type="text"
               name="address"
               required
+              disabled={isLoadingAddress}
+              defaultValue={address}
             />
+            {addressStatus === "error" && ( // if status is erorr then it will be shown
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {errorAddress}
+              </p>
+            )}
           </div>
-          <span className="absolute right-[3px] bottom-[3px] md:right-[5px] md:top-[5px] z-50">
-            <Button type="small" onClick={(e) => {
-              e.preventDefault();
-              dispatch(fetchAddress())
-            }}>
-              get postion
-            </Button>
-          </span>
+          {!position.longitude && !position.latitude && (
+            <span className="absolute bottom-[3px] right-[3px] z-50 md:right-[5px] md:top-[5px]">
+              <Button
+                disabled={isLoadingAddress}
+                type="small"
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                get postion
+              </Button>
+            </span>
+          )}
         </div>
         <div className="mb-12 flex items-center gap-5">
           <input
@@ -123,7 +143,7 @@ export async function action({ request }) {
     cart: JSON.parse(data.cart), // converted back to an  array
     priority: data.priority === "true", // will give the order a priority if the value is true
   }; // now we have data now in the shape we wanted it to be now we can use it to create new order
-    
+
   const errors = {};
   if (!isValidPhone(order.phone))
     errors.phone =
